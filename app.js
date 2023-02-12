c4963f98b8506400a2b131222b7a03231751c98b
dhruvac29 <dhruvac29@gmail.com>
2023-02-12 21:24:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+var cors = require('cors')
+
+app.use(cors())
+
+app.get('/:operator/:num1/:num2', (req, res) => {
+    const { operator, num1, num2 } = req.params;
+    let result;
+
+    switch (operator) {
+        case 'addition':
+            result = parseInt(num1) + parseInt(num2);
+            break;
+        case 'subtraction':
+            result = parseInt(num1) - parseInt(num2);
+            break;
+        case 'multiplication':
+            result = parseInt(num1) * parseInt(num2);
+            break;
+        default:
+            result = 'Invalid operator';
+    }
+
+    res.json({ result });
+});
+
+app.listen(port, () => {
+    console.log(`API server started on http://localhost:${port}`);
+});
